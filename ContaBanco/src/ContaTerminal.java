5c41e6ab0c6cc013e62aa8d8a4bb4e8f99e858b9
RudeNero <ro.nero2905@gmail.com>
2024-05-14 10:07:18 -0300

    Desafio ContaBanco feito

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // Declarar o scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        // Declarar as variáveis após a mensagem solicitada
+        System.out.println("Digite seu NOME!");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da sua AGÊNCIA!");
+        int numeroAgencia = scanner.nextInt();
+
+        System.out.println("Agora, digite o número da sua CONTA!");
+        String numeroConta = scanner.next();
+
+        System.out.println("Por último, digite seu SALDO!");
+        double saldo = scanner.nextDouble();
+
+        System.out.println(
+                "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia
+                        + ", conta " + numeroConta + " e seu saldo de R$" + saldo
+                        + " já está disponível para saque.");
+
     }
 }
